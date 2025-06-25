792298aef6759a20f674570d9a962d66a44e3f8d
ayaz-shariff <ayaz.shariff@neom.com>
2025-06-26 01:15:57 +0530

    Update 9900 main.tf

--- main.tf
+++ main.tf
@@ -49,33 +49,9 @@ output "matching_flavor_ids" {
   value = data.huaweicloud_compute_flavors.myflavor.ids
 }
 
-resource "huaweicloud_compute_instance" "basic" {
-  name               = "test-instance-terraform"
-  image_id           = "42ccabf5-f37e-42e9-833a-0927199d2340"
-  flavor_id          = "p3snl.2xlarge.8"
-  security_group_ids = ["c8ae9c7b-f1a1-40d7-b90d-bf7f61f5b302"]
-  availability_zone  = "me-east-212a"
-  system_disk_type = "SSD"  # ✅ instead of root_block_device
-  system_disk_size = 40     # in GB
-  network {
-    uuid = "06a3d364-44b8-4a34-923c-b107511629eb"
-  }
-  
-  user_data = <<EOF
-#cloud-config
-runcmd:
-  - curl -k -O 'https://hss-agent.me-east-212.managedcognitivecloud.com:10180/package/agent/linux/install/agent_Install.sh'
-  - echo 'MASTER_IP=hss-agent.me-east-212.managedcognitivecloud.com:10180' > hostguard_setup_config.conf
-  - echo 'SLAVE_IP=hss-agent-slave.me-east-212.managedcognitivecloud.com:10180' >> hostguard_setup_config.conf
-  - echo 'ORG_ID=' >> hostguard_setup_config.conf
-  - chmod +x agent_Install.sh
-  - bash agent_Install.sh
-  - rm -f agent_Install.sh
-EOF
-   
-}
 
-resource "huaweicloud_vpc" "testvpc8951" {
- name = "testvpc8951"
+
+resource "huaweicloud_vpc" "testvpc9900" {
+ name = "testvpc9900"
  cidr = "192.168.0.0/16"
 }
